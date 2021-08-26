5e8e2e1bb55a2d83d357fb6679eb3141e88d9161
pangbaiwork <15183252672@163.com>
2021-08-26 09:05:17 +0800

    add a function inesjv

---
+++ onslauncher/src/main/java/com/onscripter/exception/NativeONSException.java
@@ -0,0 +1,11 @@
+package com.onscripter.exception;
+
+public class NativeONSException extends Exception {
+
+    public NativeONSException(String message) {
+        super(message);
+    }
+
+
+    private static final long serialVersionUID = 1L;
+}
